02ae3c8ddb97969c25e7c087392e85238964e91b
Matthew Kirkley <matt.kirkley@gmail.com>
2021-11-03 14:48:30 -0400

    Removed sections from docs landing page that are relavant to skilltreeforest

--- docs/.vuepress/components/HomePage.vue
+++ docs/.vuepress/components/HomePage.vue
@@ -2,26 +2,11 @@
     <div>
         <home-page-header />
 
-        <info-cards style="margin-top: -3rem;"/>
-
-        <dashboard-section class="mt-5 mb-5"/>
-
-        <user-display-section style="margin-top: 8rem;"/>
-
-        <rapid-integration-section class="mt-5 mb-5"/>
-
-        <vue-docs-section class="mt-4 mb-0"/>
-
         <home-page-footer />
     </div>
 </template>
 
 <script>
-    import InfoCards from "./home/InfoCards";
-    import DashboardSection from "./home/DashboardSection";
-    import UserDisplaySection from "./home/UserDisplaySection";
-    import RapidIntegrationSection from "./home/RapidIntegrationSection";
-    import VueDocsSection from "./home/VueDocsSection";
     import HomePageFooter from "./home/HomePageFooter";
     import HomePageHeader from "./home/HomePageHeader";
     export default {
@@ -29,8 +14,8 @@
         components: {
             HomePageHeader,
             HomePageFooter,
-            VueDocsSection, RapidIntegrationSection, UserDisplaySection, DashboardSection, InfoCards}
-    }
+        },
+    };
 </script>
 
 <style scoped>
